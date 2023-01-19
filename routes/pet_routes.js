465ad0930ab235b009750ae2a772b28fe89fa252
Paul Truitt <paultruitt@Pauls-MacBook-Pro.local>
2023-01-18 20:40:44 -0500

    server runs, no errors

--- routes/pet_routes.js
+++ routes/pet_routes.js
@@ -4,15 +4,33 @@ const Pet = require('../models/pet')
 
 const router = express.Router()
 
-//INDEX
+// INDEX
 // GET /characters
-// router.get('/characters', (req, res, next) => {
-//     Pet.find()
-//         .then(pets => {
-//             return pets.map(pet => pet)
-//         })
-// })
+router.get('/pets', (req, res, next) => {
+    Pet.find()
+        .then(pets => {
+            return pets.map(pet => pet)
+        })
+        .then(pets => {
+            res.status(200).json({ pets: pets })
+        })
+        .catch(next)
+})
 
+// SHOW
+// GET /characters/:id
+router.get('/pets/:id', (req, res, next) => {
+    Pet.findById(req.params.id)
+        .then(pet => {
+            res.status(200).json({
+                pet: pet
+            })
+        })
+        .catch(next)
+})
+
+// create paths
+// POST /pet
 router.post('/pets', (req, res, next) => {
     Pet.create(req.body.pet)
         .then(pet => {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const cors = require('cors')
 const express = require('express')
+const cors = require('cors')
 
 const db = require('./config/db')
 const PORT = 8000
@@ -17,7 +17,7 @@ mongoose.connect(db, {
 
 const app = express()
 
-app.use(cors({ origin: `http://127.0.0.1:5500` }))
+app.use(cors({ origin: `http://127.0.0.1:5501` }))
 
 app.use(express.json())
 
